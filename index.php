a4631c6e034d3f24a5debc5f217e6d2f67f744c7
Jennifer McNiel <jennifer.mcniel@yahoo.com>
2024-05-23 14:28:09 -0400

    Added POST from form and SQL insert

--- index.php
+++ index.php
@@ -37,4 +37,37 @@ echo "<ol>";
 foreach($result as $row){
     echo "<li>".$row['last'].", ".$row['first']."</li>";
 }
-echo "</ol>";
+echo "</ol> <a href='student-form.html'>Add Student</a>";
+
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $sid = $_POST['sid'];
+    $lName = $_POST['lname'];
+    $fName = $_POST['fname'];
+    $bDay = $_POST['bday'];
+    $gpa = $_POST['gpa'];
+    $advisor = $_POST['advisor'];
+
+//    echo $sid . " " . $lName . " " . $fName . " " . $bDay . " " . $gpa . " " . $advisor;
+    //1 define the query
+    $sql = 'INSERT INTO student (sid, last, first, birthdate, gpa, advisor)
+            VALUES (:sid, :lname, :fname, :bday, :gpa, :advisor)';
+
+    // 2 Prepare the statement
+    $statement = $dbh->prepare($sql);
+
+    //3 bind the parameters
+    $statement->bindParam(':sid', $sid);
+    $statement->bindParam(':lname', $lName);
+    $statement->bindParam(':fname', $fName);
+    $statement->bindParam(':bday', $bDay);
+    $statement->bindParam(':gpa', $gpa);
+    $statement->bindParam(':advisor', $advisor);
+
+    //4 execute the query
+    $statement->execute();
+
+    //5 (optional) process the results
+    echo "<p>Student $sid was inserted successfully!</p>";
+}
