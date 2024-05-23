fca7d6091ce05517d4864b181ed11cb29f83f1af
Yadira Cervantes <cervantes.yadira@student.greenriver.edu>
2024-05-23 14:35:22 -0400

    Move form processing to insert before rendering student list

--- index.php
+++ index.php
@@ -17,30 +17,6 @@ catch (PDOException $e) {
     die ($e->getMessage()); // this prevents the code from continuing to execute
 }
 
-
-
-
-//1 define
-$sql = "SELECT * FROM student";
-
-//2 prepare statement
-$statement = $dbh->prepare($sql);
-
-//3 bind parameters N/A
-
-//4 execute query
-$statement->execute();
-
-//5 (optional) process the results
-$result = $statement->fetchAll(PDO::FETCH_ASSOC);
-echo "<ol>";
-foreach($result as $row){
-    echo "<li>".$row['last'].", ".$row['first']."</li>";
-}
-echo "</ol> <a href='student-form.html'>Add Student</a>";
-
-
-
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $sid = $_POST['sid'];
     $lName = $_POST['lname'];
@@ -71,3 +47,23 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     //5 (optional) process the results
     echo "<p>Student $sid was inserted successfully!</p>";
 }
+
+
+//1 define
+$sql = "SELECT * FROM student";
+
+//2 prepare statement
+$statement = $dbh->prepare($sql);
+
+//3 bind parameters N/A
+
+//4 execute query
+$statement->execute();
+
+//5 (optional) process the results
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+echo "<ol>";
+foreach($result as $row){
+    echo "<li>".$row['last'].", ".$row['first']."</li>";
+}
+echo "</ol> <a href='student-form.html'>Add Student</a>";
